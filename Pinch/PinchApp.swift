29e979845464675d7dddf34f1247e11472c10bb8
AbhayG21 <abhay19021@gmail.com>
2023-01-13 20:00:27 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Abhay Gupta on 13/01/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
