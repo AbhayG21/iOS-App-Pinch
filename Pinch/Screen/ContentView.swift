473f8e30ab46cb66ff2d9189110acfabc2f67ce8
AbhayG21 <abhay19021@gmail.com>
2023-01-20 22:23:25 +0530

    Drawer

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View
     @State private var isAnimating : Bool = false
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
+    @State private var isDrawerOpen: Bool = false
     // MARK: - FUNCTION
     func resetImageState()
     {
@@ -197,9 +198,29 @@ struct ContentView: View
                 HStack(spacing:12)
                 {
                     // MARK: - DRAWER HANDLE
-                    
+                    Image(systemName:isDrawerOpen ?  "chevron.compact.right" : "chevron.compact.left" )
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height:40)
+                        .padding(8)
+                        .foregroundStyle(.secondary)
+                        .onTapGesture(perform:{
+                            withAnimation(.easeOut){
+                                isDrawerOpen.toggle()
+                            }
+                        })
+                        
                     // MARK: - THUMBNAILS
+                    Spacer()
                 }
+                    .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                    .frame(width:260)
+                    .padding(.top,UIScreen.main.bounds.height/12)
+                    .offset(x:isDrawerOpen ? 20 : 215)
+                ,alignment:.topTrailing
             )//:DRAWER
             
         }//:NAVIGATION
@@ -212,6 +233,6 @@ struct ContentView_Previews: PreviewProvider
     static var previews: some View
     {
         ContentView()
-            .preferredColorScheme(.dark)
+//            .preferredColorScheme(.dark)
     }
 }
